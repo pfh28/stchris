9112f0f9e9aa3b2dd7f928d5f42a68fa33215c0e
Patrick <pfh28@users.noreply.github.com>
2023-12-09 12:45:04 -0500

    add basic working camera button and broken file display

---
+++ src/components/CameraButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <label for="cameraFileInput" class="w-8 h-8">
+        <div class="bg-gray-400 w-full h-full rounded-lg grid place-content-center cursor-pointer">
+            <div class="w-6 h-6"><CameraIcon/></div>
+        </div>
+        <input id="cameraFileInput"
+                @change="handleImage" 
+                type="file" 
+                accept="image/*" 
+                capture="environment"
+                class="invisible" />
+    </label>
+</template>
+<script setup>
+import {CameraIcon} from '@heroicons/vue/24/outline'
+
+const props = defineProps({modelValue: Object})
+
+const emit = defineEmits(['fileAdded'])
+
+function handleImage(event) {
+    emit('fileAdded', event.target.files)
+}
+</script>
--- src/components/TreasureItem.vue
+++ src/components/TreasureItem.vue
@@ -2,16 +2,14 @@
     <div class="w-full bg-gray-200 px-3 py-1 rounded-sm flex flex-col">
         <div class="flex justify-between">
             {{treasure.name}}
-            <div>
-                <button for="cameraFileInput">
-                    <span class="btn">Open camera</span>
-                    <input id="cameraFileInput" 
-                            type="file" 
-                            accept="image/*" 
-                            capture="environment"
-                            class="invisible" />
+            <div class="flex flex-row space-x-1">
+                <img class="w-8 h-8 rounded-lg" :src="answerImage"/>
+                <cameraButton @fileAdded="processFile"/>
+                <button class="w-8 h-8 bg-gray-400 rounded-lg grid place-content-center"
+                 @click="expanded = !expanded">
+                    <ChevronUpIcon class="w-5" v-if="expanded"/>
+                    <ChevronDownIcon class="w-5" v-else=""/>
                 </button>
-                <button @click="expanded = !expanded">{{expanded ? "-":"+"}}</button>
             </div>
         </div>
         <div name="details" v-if="expanded">
@@ -24,6 +22,8 @@
 <script setup>
 import {ref} from 'vue'
 import {useTreasureStore} from '../stores/treasureStore'
+import CameraButton from './CameraButton.vue'
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/vue/24/outline';
 
 const treasureStore = useTreasureStore();
 
@@ -31,7 +31,12 @@ const props = defineProps({treasure: Object})
 
 const expanded = ref(false);
 
+const answerImage = ref("")
 
+function processFile(fileList) {
+    console.log(props.treasure.name)
+    answerImage.value = window.URL.createObjectURL(fileList[0])
+}
 
 
 </script>
