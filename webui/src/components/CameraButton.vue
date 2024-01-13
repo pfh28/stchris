ee39fb76f03b2a66b7d36caf842f1b25d090be9f
Patrick <pfh28@users.noreply.github.com>
2024-01-12 22:35:09 -0500

    reorganized file structure and started django project

---
+++ webui/src/components/CameraButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <label :for="formId" class="w-8 h-8">
+        <div class="bg-gray-400 w-full h-full rounded-lg grid place-content-center cursor-pointer">
+            <div class="w-6 h-6"><CameraIcon/></div>
+        </div>
+        <input :id="formId"
+                @change="handleImage" 
+                type="file" 
+                accept="image/*" 
+                capture="environment"
+                class="invisible" />
+    </label>
+</template>
+<script setup>
+import {CameraIcon} from '@heroicons/vue/24/outline'
+
+// prevents all buttons from having the same ID
+const formId = Math.floor(Math.random()*1000000)
+
+const emit = defineEmits(['fileAdded'])
+
+function handleImage(event) {
+    emit('fileAdded', event.target.files)
+}
+</script>
---
+++ webui/src/views/TreasureList.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+  <div name="listContainer" class="w-full h-full space-y-2">
+    <TreasureItem v-for="treasure in treasureList" :key="treasure.id" 
+        :treasure="treasure"/>
+  </div>
+  </main>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import {useTreasureStore} from '../stores/treasureStore'
+import TreasureItem from '../components/TreasureItem.vue';
+
+const treasureStore = useTreasureStore();
+
+const {treasureList} = storeToRefs(treasureStore)
+
+console.log(treasureList);
+
+treasureStore.getTreasureList();
+
+</script>
